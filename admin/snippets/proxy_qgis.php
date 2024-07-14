b4e62543866b35f12224feb3d4d6290f4e9a2c9d
David Ghedini <david@davidghedini.com>
2024-07-15 02:08:03 +0300

    Update proxy_qgis.php
    Remove comments

--- admin/snippets/proxy_qgis.php
+++ admin/snippets/proxy_qgis.php
@@ -3,9 +3,6 @@
 	const QGIS_FILENAME = 'QGIS_FILE_VALUE';
 	include('../../admin/incl/index_prefix.php');
 
-	// https://demo.qualgis.com/cgi-bin/qgis_mapserv.fcgi?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities&map=/home/david1/firstname/front.qgs
-	// https://demo.qualgis.com/apps/17/proxy_pdf.php?SERVICE=WMS&REQUEST=GetPrint&CRS=EPSG:4326&FORMAT=pdf&CRS=EPSG:3857&TEMPLATE=newbee&map0:EXTENT=1029311,5909796,1032022,5911468
-	
 	$format = '';
 	if(preg_match('/FORMAT=([a-z]+)&/',$_SERVER['QUERY_STRING'], $matches)){
 		$format = $matches[1];
