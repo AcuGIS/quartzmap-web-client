7e216d6e3fb97d795256c7f7401b48504e4f0b0b
David Ghedini <david@davidghedini.com>
2024-07-15 18:58:28 +0300

    Update index.php
    Remove comments

--- admin/index.php
+++ admin/index.php
@@ -74,20 +74,14 @@ text-decoration:none!important;
                 class="card bg-info text-white w-100 card-hover"
               >
 <div class="card-header pt-5">
-        <!--begin::Title-->
         <div class="card-title d-flex flex-column">   
-            <!--begin::Amount-->
             <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.598-.49L10.5.99 5.598.01a.5.5 0 0 0-.196 0l-5 1A.5.5 0 0 0 0 1.5v14a.5.5 0 0 0 .598.49l4.902-.98 4.902.98a.5.5 0 0 0 .196 0l5-1A.5.5 0 0 0 16 14.5zM5 14.09V1.11l.5-.1.5.1v12.98l-.402-.08a.5.5 0 0 0-.196 0zm5 .8V1.91l.402.08a.5.5 0 0 0 .196 0L11 1.91v12.98l-.5.1z"/>
 </svg>
  Maps</span>
-            <!--end::Amount-->
 
-            <!--begin::Subtitle-->
             <span class="text-white opacity-75 pt-1 fw-semibold fs-6"></span>             
-            <!--end::Subtitle--> 
         </div>
-        <!--end::Title-->         
     </div>
                 <div class="card-body" style="box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);">
                   <div class="d-flex align-items-center">
@@ -113,18 +107,12 @@ text-decoration:none!important;
                 class="card bg-success text-white w-100 card-hover"
               >
 <div class="card-header pt-5">
-        <!--begin::Title-->
         <div class="card-title d-flex flex-column">   
-            <!--begin::Amount-->
             <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2"><i class="mdi mdi-database"></i>
  Data Sources</span>
-            <!--end::Amount-->
 
-            <!--begin::Subtitle-->
             <span class="text-white opacity-75 pt-1 fw-semibold fs-6"></span>             
-            <!--end::Subtitle--> 
         </div>
-        <!--end::Title-->         
     </div>
                 <div class="card-body" style="box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);">
                   <div class="d-flex align-items-center">
@@ -149,19 +137,13 @@ text-decoration:none!important;
               >
 
 <div class="card-header pt-5">
-        <!--begin::Title-->
         <div class="card-title d-flex flex-column">   
-            <!--begin::Amount-->
             <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
   <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
 </svg> User Groups</span>
-            <!--end::Amount-->
 
-            <!--begin::Subtitle-->
             <span class="text-white opacity-75 pt-1 fw-semibold fs-6"></span>             
-            <!--end::Subtitle--> 
         </div>
-        <!--end::Title-->         
     </div>
                 <div class="card-body" style="box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);">
                   <div class="d-flex align-items-center">
@@ -266,36 +248,7 @@ text-decoration:none!important;
                 </div>
               </a>
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             </div>
-
-
-
-
-
           </div>
                
             </div>
