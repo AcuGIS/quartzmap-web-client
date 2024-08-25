cefc189738a5cebaeadda4b6e5870c4bc4793ba8
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2024-08-25 13:23:24 +0300

    fix content-type value in header

--- admin/snippets/data_filep.php
+++ admin/snippets/data_filep.php
@@ -6,7 +6,7 @@ $f = str_replace('..', '', $_GET['f']);
 $fpath = DATA_DIR.'/'.MAP_ID.'/'.$f;
 
 if(!empty($_GET['f']) && is_file($fpath)){
-	header('Content-Type: ').mime_content_type($fpath);
+	header('Content-Type: '.mime_content_type($fpath));
 	readfile($fpath);
 }else{
 	header("HTTP/1.1 400 Bad Request");
