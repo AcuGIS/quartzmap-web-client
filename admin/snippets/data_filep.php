cf9197fa16b41cce3a583f9e1d4f4db2d261f346
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2024-09-12 16:36:01 +0300

    fix data_filep download name

--- admin/snippets/data_filep.php
+++ admin/snippets/data_filep.php
@@ -7,6 +7,7 @@ $fpath = DATA_DIR.'/'.MAP_ID.'/'.$f;
 
 if(!empty($_GET['f']) && is_file($fpath)){
 	header('Content-Type: '.mime_content_type($fpath));
+	header('Content-disposition: attachment; filename="'.$f.'"');
 	readfile($fpath);
 }else{
 	header("HTTP/1.1 400 Bad Request");
